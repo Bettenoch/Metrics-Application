96cbe6c7d9c726a1d6636620c98b1b197539c80e
Bett Enock <bettenock968@gmail.com>
2023-09-15 12:20:19 -0700

    configure redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
 
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 
   </Provider>,
 );
