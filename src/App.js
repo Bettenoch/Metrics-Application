99fa550d082f190095f3613015077e4f4a7f1b43
Bett Enock <bettenock968@gmail.com>
2023-09-15 12:22:08 -0700

    style page elements

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { getRockets } from './redux/rockets/rocketSlice';
-import DisplayContent from './component/Missions/DisplayContent';
-import NavBar from './component/NavBar';
-import Rocket from './component/Rockets/Rocket';
-import MyProfile from './component/MyProfile/MyProfile';
-import { fetchMission } from './redux/missionSlice/Mission';
-import './App.css';
-import './component/Missions/Mission.module.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import WeatherData from './component/WeatherData';
+import TownInfo from './component/townInfo';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getRockets());
-  });
-  
-  useEffect(() => {
-    dispatch(fetchMission());
-  }, [dispatch]);
-
   return (
     <div className="App">
 
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Rocket />} />
-          <Route path="Mission" element={<DisplayContent />} />
-          <Route path="MyProfile" element={<MyProfile />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route index element={<WeatherData />} />
+        <Route path="/city/:townName" element={<TownInfo />} />
+      </Routes>
+
     </div>
   );
 }
