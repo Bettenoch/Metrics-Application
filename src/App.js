4586f8089229721a197028a0932c3f87606ae710
Bett Enock <bettenock968@gmail.com>
2023-09-16 08:22:06 -0700

    add change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import { fetchData } from './redux/AppSlice';
 import WeatherData from './component/WeatherData';
 import TownInfo from './component/townInfo';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
   return (
     <div className="App">
 
